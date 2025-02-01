ad4dbe0d4c3b127024b5405f6f02221bda61c117
crgarner99 <cameron.r.garner@gmail.com>
2025-01-31 21:52:58 -0700

    second commit with Eclipse file

---
+++ Week3GitCommit/src/githubCommit/Week3Commit.java
@@ -0,0 +1,10 @@
+package githubCommit;
+
+public class Week3Commit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Week 3 Commit on Github");
+	}
+
+}
